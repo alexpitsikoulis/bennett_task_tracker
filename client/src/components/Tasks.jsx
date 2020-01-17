7674d17925952a4baea374d20b53eedb3fc29c5d
Alex Pitsikoulis <APitsikoulis@BG.intra>
2020-01-17 15:36:06 -0500

    added functionality to sort tasks by priority level

--- client/src/components/Tasks.jsx
+++ client/src/components/Tasks.jsx
@@ -3,39 +3,80 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 
 export default class Tasks extends Component {
+  priorityFilterAndMap = p => {
+    return this.props.tasks
+      .filter(task => {
+        return task.priority === p;
+      })
+      .map(task => {
+        let priorityColor = "";
+        switch (task.priority) {
+          case "Low":
+            priorityColor = "rgba(0, 255, 0, 0.8)";
+            break;
+          case "Medium":
+            priorityColor = "rgba(255, 255, 0, 0.8)";
+            break;
+          case "High":
+            priorityColor = "rgba(255, 0, 0, 0.8)";
+            break;
+          default:
+            priorityColor = "white";
+        }
+        return (
+          <Link
+            to={`/${task.userId}/tasks/${task._id}`}
+            style={{ color: "black", textDecoration: "none" }}
+          >
+            <div className="task" style={{ backgroundColor: priorityColor }}>
+              <h3>{task.title}</h3>
+              <h4>Priority: {task.priority}</h4>
+              <h4>Est. Hours To Complete: {task.estimatedHours}</h4>
+            </div>
+          </Link>
+        );
+      });
+  };
   render() {
-    const taskList = this.props.tasks.map(task => {
-      let priorityColor = "";
-      switch (task.priority) {
-        case "Low":
-          priorityColor = "rgba(0, 255, 0, 0.8)";
-          break;
-        case "Medium":
-          priorityColor = "rgba(255, 255, 0, 0.8)";
-          break;
-        case "High":
-          priorityColor = "rgba(255, 0, 0, 0.8)";
-          break;
-        default:
-          priorityColor = "white";
-      }
-      return (
-        <Link to={`/${task.userId}/tasks/${task._id}`}>
-          <div className="task" style={{ backgroundColor: priorityColor }}>
-            <h3>{task.title}</h3>
-            <h4>Priority: {task.priority}</h4>
-            <h4>Est. Hours To Complete: {task.estimatedHours}</h4>
-          </div>
-        </Link>
-      );
-    });
+    // const taskList = this.props.tasks.map(task => {
+    //   let priorityColor = "";
+    //   switch (task.priority) {
+    //     case "Low":
+    //       priorityColor = "rgba(0, 255, 0, 0.8)";
+    //       break;
+    //     case "Medium":
+    //       priorityColor = "rgba(255, 255, 0, 0.8)";
+    //       break;
+    //     case "High":
+    //       priorityColor = "rgba(255, 0, 0, 0.8)";
+    //       break;
+    //     default:
+    //       priorityColor = "white";
+    //   }
+    //   return (
+    //     <Link
+    //       to={`/${task.userId}/tasks/${task._id}`}
+    //       style={{ color: "black", textDecoration: "none" }}
+    //     >
+    //       <div className="task" style={{ backgroundColor: priorityColor }}>
+    //         <h3>{task.title}</h3>
+    //         <h4>Priority: {task.priority}</h4>
+    //         <h4>Est. Hours To Complete: {task.estimatedHours}</h4>
+    //       </div>
+    //     </Link>
+    //   );
+    // });
     return (
       <div>
         <h2>Tasks</h2>
         <Link to={`/${this.props.userId}/newtask`}>
           <button>Create New Task</button>
         </Link>
-        <div className="task-list">{taskList}</div>
+        <div className="task-list">
+          {this.priorityFilterAndMap("High")}
+          {this.priorityFilterAndMap("Medium")}
+          {this.priorityFilterAndMap("Low")}
+        </div>
       </div>
     );
   }
