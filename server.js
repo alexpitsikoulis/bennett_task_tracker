1c0496fb371681ada235956677555376da761f22
Alex Pitsikoulis <alex.pitsikoulis@gmail.com>
2020-02-11 14:31:29 -0500

    commented out the scheduler code since heroku scheduler is now taking care of it

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ require("dotenv/config");
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const axios = require("axios");
+// const axios = require("axios");
 const { userRouter } = require("./controllers/User");
 const { taskRouter } = require("./controllers/Task");
 const { allTasksRouter } = require("./controllers/GetAllTasks");
 const { mailRouter } = require("./controllers/Mail");
 const { fileRouter } = require("./controllers/File");
 const passport = require("passport");
-const schedule = require("node-schedule");
+// const schedule = require("node-schedule");
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -40,41 +40,41 @@ app.get("/*", (req, res) => {
 
 const PORT = process.env.PORT || 3001;
 
-schedule.scheduleJob("0 8 * * *", () => {
-  axios.get(`http://localhost:${PORT}/api/tasks`).then(tasks => {
-    const unfinishedTasksDueToday = tasks.data.filter(task => {
-      const year = new Date(task.dueDate).getFullYear();
-      const month = new Date(task.dueDate).getMonth();
-      const date = new Date(task.dueDate).getDate();
-      const today = new Date(Date.now());
+// schedule.scheduleJob("0 8 * * *", () => {
+//   axios.get(`http://localhost:${PORT}/api/tasks`).then(tasks => {
+//     const unfinishedTasksDueToday = tasks.data.filter(task => {
+//       const year = new Date(task.dueDate).getFullYear();
+//       const month = new Date(task.dueDate).getMonth();
+//       const date = new Date(task.dueDate).getDate();
+//       const today = new Date(Date.now());
 
-      return (
-        year === today.getFullYear() &&
-        month === today.getMonth() &&
-        date === today.getDate() &&
-        task.status !== "Finished"
-      );
-    });
+//       return (
+//         year === today.getFullYear() &&
+//         month === today.getMonth() &&
+//         date === today.getDate() &&
+//         task.status !== "Finished"
+//       );
+//     });
 
-    unfinishedTasksDueToday.forEach(task => {
-      const subject = `Reminder: You Have Unfinished Tasks Due Today`;
-      const email = task.userEmail;
-      const message = `Your task ${task.title} is due today and has not yet been marked completed.`;
-      axios
-        .post(`http://localhost:${PORT}/send`, {
-          subject,
-          email,
-          message
-        })
-        .then(() => {
-          console.log("message sent successfully");
-        })
-        .catch(err => {
-          console.log(err);
-        });
-    });
-  });
-});
+//     unfinishedTasksDueToday.forEach(task => {
+//       const subject = `Reminder: You Have Unfinished Tasks Due Today`;
+//       const email = task.userEmail;
+//       const message = `Your task ${task.title} is due today and has not yet been marked completed.`;
+//       axios
+//         .post(`http://localhost:${PORT}/send`, {
+//           subject,
+//           email,
+//           message
+//         })
+//         .then(() => {
+//           console.log("message sent successfully");
+//         })
+//         .catch(err => {
+//           console.log(err);
+//         });
+//     });
+//   });
+// });
 
 app.listen(PORT, () => {
   console.log(`App is listening on PORT ${PORT}`);
